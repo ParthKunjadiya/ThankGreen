36484102c60071138e4635920a2d6ac1b790d67c
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-04-05 18:07:24 +0530

    repository modified

--- repository/address.js
+++ repository/address.js
@@ -2,7 +2,7 @@ const db = require('../util/database');
 
 const insertAddress = async ({ userId, address_type, address, state, country, zip_code, latitude, longitude }) => {
     return await db.execute(
-        'INSERT INTO ThankGreen.address (user_id, address_type, address, state, country, zip_code, latitude, longitude ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+        'INSERT INTO address (user_id, address_type, address, state, country, zip_code, latitude, longitude ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
         [userId, address_type, address, state, country, zip_code, latitude, longitude]
     )
 }
@@ -11,18 +11,18 @@ const getAddress = async (key) => {
     const keys = Object.keys(key);
     const values = Object.values(key);
 
-    let sql = 'SELECT * FROM ThankGreen.address WHERE ';
+    let sql = 'SELECT * FROM address WHERE ';
     sql += keys.map(key => `${key} = ?`).join(' AND ');
 
     return await db.execute(sql, values);
 }
 
 const updateAddress = async ({ userId, address_id, address_type, address, state, country, zip_code, latitude, longitude }) => {
-    return await db.query("UPDATE ThankGreen.address SET address_type = ?, address = ?, state = ?, country=?, zip_code = ?, latitude = ?, longitude = ? WHERE (id=? AND user_id = ?)", [address_type, address, state, country, zip_code, latitude, longitude, address_id, userId])
+    return await db.query("UPDATE address SET address_type = ?, address = ?, state = ?, country=?, zip_code = ?, latitude = ?, longitude = ? WHERE (id=? AND user_id = ?)", [address_type, address, state, country, zip_code, latitude, longitude, address_id, userId])
 }
 
 const deleteAddress = async ({ userId, address_id }) => {
-    return await db.query("DELETE FROM ThankGreen.address WHERE (id = ? AND user_id = ?)", [address_id, userId])
+    return await db.query("DELETE FROM address WHERE (id = ? AND user_id = ?)", [address_id, userId])
 }
 
 module.exports = {
--- repository/user.js
+++ repository/user.js
@@ -2,7 +2,7 @@ const db = require('../util/database');
 
 const insertUser = async (profileImageUrl, name, email, password, countryCode, phoneNumber) => {
     return await db.execute(
-        'INSERT INTO ThankGreen.users (name, email, password, profileImageUrl, country_code, phone_number, referral_code) VALUES (?, ?, ?, ?, ?, ?, ?)',
+        'INSERT INTO users (name, email, password, profileImageUrl, country_code, phone_number, referral_code) VALUES (?, ?, ?, ?, ?, ?, ?)',
         [name, email, password, profileImageUrl, countryCode, phoneNumber, 1234]
     ).then(result => {
         // Extract the auto-generated ID from the result object
@@ -17,30 +17,30 @@ const getUserData = async (key) => {
     const keys = Object.keys(key);
     const values = Object.values(key);
 
-    let sql = 'SELECT * FROM ThankGreen.users WHERE ';
+    let sql = 'SELECT * FROM users WHERE ';
     sql += keys.map(key => `${key} = ?`).join(' AND ');
 
     return await db.execute(sql, values);
 }
 
 const updateUserProfileImage = async ({ userId, profileImageUrl }) => {
-    return await db.execute(`UPDATE ThankGreen.users SET profileImageUrl = ? WHERE (id = ?)`, [profileImageUrl, userId])
+    return await db.execute(`UPDATE users SET profileImageUrl = ? WHERE (id = ?)`, [profileImageUrl, userId])
 }
 
 const updateUserData = async ({ userId, name, email, phone_number }) => {
-    return await db.execute(`UPDATE ThankGreen.users SET name = ?, email = ?, phone_number = ? WHERE (id = ?)`, [name, email, phone_number, userId])
+    return await db.execute(`UPDATE users SET name = ?, email = ?, phone_number = ? WHERE (id = ?)`, [name, email, phone_number, userId])
 }
 
 const updateUserPassword = async (userId, hashedNewPassword) => {
-    return await db.execute(`UPDATE ThankGreen.users SET password = ? WHERE (id = ?)`, [hashedNewPassword, userId])
+    return await db.execute(`UPDATE users SET password = ? WHERE (id = ?)`, [hashedNewPassword, userId])
 }
 
 const setResetTokenToUser = async (email, resetToken, resetTokenExpiry) => {
-    return await db.execute(`UPDATE ThankGreen.users SET resetToken = ?, resetTokenExpiry = ? WHERE (email = ?)`, [resetToken, resetTokenExpiry, email])
+    return await db.execute(`UPDATE users SET resetToken = ?, resetTokenExpiry = ? WHERE (email = ?)`, [resetToken, resetTokenExpiry, email])
 }
 
 const updatePasswordAndToken = async (hashedNewPassword, userId) => {
-    return await db.query("UPDATE ThankGreen.users SET password = ?, resetToken = NULL, resetTokenExpiry = NULL where id=?", [hashedNewPassword, userId])
+    return await db.query("UPDATE users SET password = ?, resetToken = NULL, resetTokenExpiry = NULL where id=?", [hashedNewPassword, userId])
 }
 
 module.exports = {
