0de2fa8e65f8a3bd3c59ae77d30521c9210350f1
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-04-08 12:11:07 +0530

    card db function changed

--- repository/card.js
+++ repository/card.js
@@ -2,7 +2,7 @@ const db = require('../util/database');
 
 const insertCard = async ({ userId, number, holder_name, expiry, cvv }) => {
     return await db.execute(
-        'INSERT INTO address (user_id, number, holder_name, expiry, cvv) VALUES (?, ?, ?, ?, ?, ?)',
+        'INSERT INTO cards (user_id, number, holder_name, expiry, cvv) VALUES (?, ?, ?, ?, ?, ?)',
         [userId, number, holder_name, expiry, cvv]
     )
 }
@@ -11,18 +11,18 @@ const getCard = async (key) => {
     const keys = Object.keys(key);
     const values = Object.values(key);
 
-    let sql = 'SELECT * FROM address WHERE ';
+    let sql = 'SELECT * FROM cards WHERE ';
     sql += keys.map(key => `${key} = ?`).join(' AND ');
 
     return await db.execute(sql, values);
 }
 
 const updateCard = async ({ userId, card_id, number, holder_name, expiry, cvv }) => {
-    return await db.query("UPDATE address SET number = ?, holder_name = ?, expiry = ?, cvv=? WHERE (id=? AND user_id = ?)", [number, holder_name, expiry, cvv, card_id, userId])
+    return await db.query("UPDATE cards SET number = ?, holder_name = ?, expiry = ?, cvv=? WHERE (id=? AND user_id = ?)", [number, holder_name, expiry, cvv, card_id, userId])
 }
 
 const deleteCard = async ({ userId, card_id }) => {
-    return await db.query("DELETE FROM address WHERE (id = ? AND user_id = ?)", [card_id, userId])
+    return await db.query("DELETE FROM cards WHERE (id = ? AND user_id = ?)", [card_id, userId])
 }
 
 module.exports = {
