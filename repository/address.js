1a432b3b8d936d5fe511d1eefad8e5c58c0852b6
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-04-30 15:26:09 +0530

    address api modify

--- repository/address.js
+++ repository/address.js
@@ -1,9 +1,9 @@
 const db = require('../util/database');
 
-const insertAddress = async ({ user_id, address_type, address, state, country, zip_code, latitude, longitude }) => {
+const insertAddress = async ({ user_id, address_type, address, landmark, zip_code, latitude, longitude }) => {
     let sql = `INSERT INTO address SET ?`
 
-    let params = { user_id, address_type, address, state, country, zip_code, latitude, longitude }
+    let params = { user_id, address_type, address, landmark, zip_code, latitude, longitude }
     return await db.query(sql, params)
 }
 
@@ -11,16 +11,16 @@ const getAddress = async (key) => {
     const keys = Object.keys(key);
     const values = Object.values(key);
 
-    let sql = `SELECT id, address_type, address, state, country, zip_code, latitude, longitude FROM address WHERE `;
+    let sql = `SELECT id, address_type, address, landmark, zip_code, latitude, longitude FROM address WHERE `;
     sql += keys.map(key => `${key} = ?`).join(' AND ');
 
     return await db.query(sql, values);
 }
 
-const updateAddress = async ({ userId, address_id, address_type, address, state, country, zip_code, latitude, longitude }) => {
-    let sql = `UPDATE address SET address_type = ?, address = ?, state = ?, country=?, zip_code = ?, latitude = ?, longitude = ? WHERE (id=? AND user_id = ?)`
+const updateAddress = async ({ userId, address_id, address_type, address, landmark, zip_code, latitude, longitude }) => {
+    let sql = `UPDATE address SET address_type = ?, address = ?, landmark = ?, zip_code = ?, latitude = ?, longitude = ? WHERE (id=? AND user_id = ?)`
 
-    let params = [address_type, address, state, country, zip_code, latitude, longitude, address_id, userId]
+    let params = [address_type, address, landmark, zip_code, latitude, longitude, address_id, userId]
     return await db.query(sql, params)
 }
 
