d318202de3fdd1e3f0073c2769c4aa4016d60e68
ParthKunjadiya <parthkunjadiya3@gmail.com>
2024-06-19 16:04:29 +0530

    resetPasswordLink change as frontend

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -526,7 +526,7 @@ exports.resetPasswordLink = (req, res, next) => {
                 subject: 'Reset Password',
                 html: `
                     <p>You requested a password reset</p>
-                    <p>Click this <a href="https://thankgreen.onrender.com/api/auth/reset/${resetToken}">Link</a> to set a new password.</p>
+                    <p>Click this <a href="thankGreenApp://resetpassword/${resetToken}">Link</a> to set a new password.</p>
                 `
             };
             transporter.sendMail(mailOptions, function (err, info) {
